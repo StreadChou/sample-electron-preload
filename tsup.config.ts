63ae71e0070612110040b2ff62a012bd61d5cdc9
stread <stread.chou@gmail.com>
2025-08-12 10:18:56 +0800

    处理导入导出BUG

---
+++ tsup.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig([
+    {
+        entry: ['src/main/index.ts'], // 每个入口点都可以自定义
+        outDir: 'dist/main',
+        format: ['cjs', 'esm'],
+        dts: true,
+        splitting: false,
+        sourcemap: true,
+        clean: true,
+    },
+    {
+        entry: ['src/preload/index.ts'],
+        outDir: 'dist/preload',
+        format: ['cjs', 'esm'],
+        dts: true,
+        splitting: false,
+        sourcemap: true,
+        clean: true,
+    },
+    {
+        entry: ['src/renderer/index.ts'],
+        outDir: 'dist/renderer',
+        format: ['cjs', 'esm'],
+        dts: true,
+        splitting: false,
+        sourcemap: true,
+        clean: true,
+    }
+]);
